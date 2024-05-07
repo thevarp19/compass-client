a93c663106038a3fff492413e5795b48705a57e6
Yerlan Jumabayev <thevarp18@gmail.com>
2024-05-08 00:35:16 +0500

    our-projects page mobile

--- src/modules/our-projects/OurProjects.tsx
+++ src/modules/our-projects/OurProjects.tsx
@@ -3,16 +3,16 @@ import { FC } from "react";
 
 export const OurProjects: FC = () => {
     return (
-        <div className="bg-gray flex flex-col gap-10 px-[146px] py-[60px] text-black">
-            <h2 className="text-[32px] font-medium leading-[130%]">
+        <div className="bg-gray flex flex-col gap-5 sm:gap-10 py-10 sm:py-20 px-[25px] sm:px-[146px] text-black">
+            <h2 className="text-[15px] sm:text-[32px] font-medium leading-[130%]">
                 Наши Проекты
             </h2>
             <div className="flex flex-col gap-5">
                 <div className="flex flex-col gap-[10px]">
-                    <h2 className="text-[24px] font-medium leading-[130%]">
+                    <h2 className="text-[12px] sm:text-[24px] font-medium leading-[130%]">
                         2021-2022 “5:32” (Salem Social Media)
                     </h2>
-                    <h2 className="text-[18px] font-medium leading-[130%]">
+                    <h2 className="text-[9px] sm:text-[18px] font-medium leading-[130%]">
                         В главных ролях:{" "}
                         <span className="text-[#6E9CF2]">
                             Абильмансур Сериков
@@ -21,19 +21,19 @@ export const OurProjects: FC = () => {
                 </div>
                 <Image
                     src={"/images/532banner.png"}
-                    width={1148}
-                    height={550}
+                    width={380}
+                    height={190}
                     alt="5:32"
-                    className="w-[1148px] h-[550px] rounded-[9px] "
+                    className="w-[380px] h-[190px] sm:w-[1148px] sm:h-[550px] rounded-[9px] "
                     style={{ objectFit: "cover", objectPosition: "center" }}
                 />
             </div>
             <div className="flex flex-col gap-5">
                 <div className="flex flex-col gap-[10px]">
-                    <h2 className="text-[24px] font-medium leading-[130%]">
+                    <h2 className="text-[12px] sm:text-[24px] font-medium leading-[130%]">
                         2021-2022 “5:32” (Salem Social Media)
                     </h2>
-                    <h2 className="text-[18px] font-medium leading-[130%]">
+                    <h2 className="text-[9px] sm:text-[18px] font-medium leading-[130%]">
                         В главных ролях:{" "}
                         <span className="text-[#6E9CF2]">
                             Абильмансур Сериков
@@ -42,19 +42,19 @@ export const OurProjects: FC = () => {
                 </div>
                 <Image
                     src={"/images/532banner.png"}
-                    width={1148}
-                    height={550}
+                    width={380}
+                    height={190}
                     alt="5:32"
-                    className="w-[1148px] h-[550px] rounded-[9px] "
+                    className="w-[380px] h-[190px] sm:w-[1148px] sm:h-[550px] rounded-[9px] "
                     style={{ objectFit: "cover", objectPosition: "center" }}
                 />
             </div>
             <div className="flex flex-col gap-5">
                 <div className="flex flex-col gap-[10px]">
-                    <h2 className="text-[24px] font-medium leading-[130%]">
+                    <h2 className="text-[12px] sm:text-[24px] font-medium leading-[130%]">
                         2021-2022 “5:32” (Salem Social Media)
                     </h2>
-                    <h2 className="text-[18px] font-medium leading-[130%]">
+                    <h2 className="text-[9px] sm:text-[18px] font-medium leading-[130%]">
                         В главных ролях:{" "}
                         <span className="text-[#6E9CF2]">
                             Абильмансур Сериков
@@ -63,10 +63,10 @@ export const OurProjects: FC = () => {
                 </div>
                 <Image
                     src={"/images/532banner.png"}
-                    width={1148}
-                    height={550}
+                    width={380}
+                    height={190}
                     alt="5:32"
-                    className="w-[1148px] h-[550px] rounded-[9px] "
+                    className="w-[380px] h-[190px] sm:w-[1148px] sm:h-[550px] rounded-[9px] "
                     style={{ objectFit: "cover", objectPosition: "center" }}
                 />
             </div>
