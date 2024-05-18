6a6676f5670ae4433523e1ea2826bdf26341e846
Abzal Slamkozha <99246260+guinnod@users.noreply.github.com>
2024-05-18 05:14:44 +0500

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,1 +1,1 @@
-export const baseURL = process.env.NEXT_PUBLIC_BACKEND_URL || "";
+export const baseURL = "https://nestjs-compass-api-bvrrx45lva-uc.a.run.app/" // process.env.NEXT_PUBLIC_BACKEND_URL || "";
